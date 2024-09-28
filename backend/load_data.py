f8fabb78ab9231a1f008831ebebf1461cb3e6ebf
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2024-09-28 16:31:24 +0200

    feat: add inventory initial data to db

--- backend/load_data.py
+++ backend/load_data.py
@@ -1,7 +1,39 @@
 import csv
+from database.dbconnect import SessionLocal
+from database.schemas.Inventory import InventorySchema
 
-def load_inventory_info():
+dbHandler = SessionLocal()
+
+
+async def load_inventory_info():
     with open("data/inventory.csv", mode="r") as file:
         inventory_data = csv.DictReader(file)
-        column_names = file.readline().strip().split(',')
-        
+        column_names = file.readline().strip().split(",")
+
+        for data in inventory_data:
+            new_inventory = InventorySchema(
+                product_number=data[column_names[0]],
+                material=data[column_names[1]],
+                form=data[column_names[2]],
+                choice=data[column_names[3]],
+                grade=data[column_names[4]],
+                finish=data[column_names[5]],
+                surface=data[column_names[6]],
+                quantity=data[column_names[7]],
+                weight=data[column_names[8]],
+                length=data[column_names[9]],
+                width=data[column_names[10]],
+                height=data[column_names[11]],
+                thickness=data[column_names[12]],
+                outer_diameter=data[column_names[13]],
+                wall_thickness=data[column_names[14]],
+                web_thickness=data[column_names[15]],
+                flange_thickness=data[column_names[16]],
+                certificates=data[column_names[17]],
+                Location=data[column_names[19]],
+            )
+            dbHandler.add(new_inventory)
+            await dbHandler.commit()
+            await dbHandler.refresh()
+
+    print("All inventory data was inserted")
--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,9 @@
 from app import app_server
+from dotenv import load_dotenv
 from load_data import load_inventory_info
 
 if __name__ == "__main__":
-    load_inventory_info()
+    load_dotenv()
 
+    load_inventory_info()
     app_server.run(debug=False, port=5000)
