5680c56370365d7e57c93632acc2dcb93f2c8849
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2024-09-29 19:26:43 +0200

    fix: db init create table

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,14 @@
-from app import app_server
-from dotenv import load_dotenv
+import asyncio
+from app import app_server, dbHandler
 from load_data import load_inventory_info
 
 if __name__ == "__main__":
-    load_dotenv()
-
-    load_inventory_info()
+    asyncio.run(load_inventory_info())
+    # load_inventory_info()
+    with app_server.app_context():
+        try:
+            dbHandler.create_all()
+        except Exception as e:
+            print ("DB initialize failed!")
+            pass
     app_server.run(debug=False, port=5000)
