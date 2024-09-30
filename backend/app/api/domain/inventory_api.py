047424ac107e59f96e34ec9b42c55080e126733c
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2024-09-30 20:18:51 +0200

    fix: rename retrival service func

--- backend/app/api/domain/inventory_api.py
+++ backend/app/api/domain/inventory_api.py
@@ -6,6 +6,6 @@ inventory_api = APIRouter()
 
 
 @inventory_api.get("/")
-async def health_check(page: int = Query(1, ge=1), perpage: int = Query(10, ge=5)):
+async def get_inventory_data(page: int = Query(1, ge=1), perpage: int = Query(10, ge=5)):
     result = await inventoryService.get_inventory_data(page=page, perpage=perpage)
     return result
