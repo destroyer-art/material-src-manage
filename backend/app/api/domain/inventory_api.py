73b2abca285b0ba48ce411add43403d3c66bda80
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2024-09-30 20:28:34 +0200

    fix: format code

--- backend/app/api/domain/inventory_api.py
+++ backend/app/api/domain/inventory_api.py
@@ -6,6 +6,8 @@ inventory_api = APIRouter()
 
 
 @inventory_api.get("/")
-async def get_inventory_data(page: int = Query(1, ge=1), perpage: int = Query(10, ge=5)):
+async def get_inventory_data(
+    page: int = Query(1, ge=1), perpage: int = Query(10, ge=5)
+):
     result = await inventoryService.get_inventory_data(page=page, perpage=perpage)
     return result
