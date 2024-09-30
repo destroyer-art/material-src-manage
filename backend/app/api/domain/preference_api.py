a2e84e8579e23176b66752a0ecdf227105bbd3d5
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2024-09-30 20:13:04 +0200

    feat: preference data retrival and insert

--- backend/app/api/domain/preference_api.py
+++ backend/app/api/domain/preference_api.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter, Query
 from app.services import preferenceService
+from app.models.preference import PreferenceModel
+
+from typing import List
 
 preference_api = APIRouter()
 
@@ -8,3 +11,10 @@ preference_api = APIRouter()
 async def get_preference_info(id: int = Query(default=None, ge=1)):
     result = await preferenceService.get_preference_data(id)
     return result
+
+
+@preference_api.post("/")
+async def get_prefer_match_data(preferences: List[PreferenceModel]):
+    for prefer in preferences:
+        await preferenceService.insert_data(prefer)
+    return {"Status": "All data inserted"}
