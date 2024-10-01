0eabf55dd51f3aa117acd1b7bb54067f64478a8e
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2024-10-01 13:29:58 +0200

    feat: store preference data on db

--- frontend/src/components/Common/UploadPrefer.tsx
+++ frontend/src/components/Common/UploadPrefer.tsx
@@ -1,26 +1,47 @@
 import { UploadFile } from "@mui/icons-material";
 import { Box, Button } from "@mui/material";
+import axios from "axios";
 import React, { ChangeEvent, useState } from "react";
+import { Preference } from "../../types";
 
 interface CSVData {
-    [key: string]: string;
-  }
-  
-  const parseCSV = (csvText: string): CSVData[] => {
-    const lines = csvText.split("\n").map((line) => line.trim());
-    if (lines.length < 2) return [];
-  
-    const headers = lines[0].split(",");
-    const rows: CSVData[] = lines.slice(1).map((line) => {
-      const values = line.split(",");
-      return headers.reduce<CSVData>((acc, header, index) => {
-        acc[header.trim()] = values[index]?.trim() || "";
-        return acc;
-      }, {});
-    });
-  
-    return rows;
-  };
+  [key: string]: string;
+}
+
+const parseCSV = (csvText: string): CSVData[] => {
+  const lines = csvText.split("\n").map((line) => line.trim());
+  if (lines.length < 2) return [];
+
+  const headers = lines[0].split(",");
+  const rows: CSVData[] = lines.slice(1).map((line) => {
+    const values = line.split(",");
+    return headers.reduce<CSVData>((acc, header, index) => {
+      acc[header.trim()] = values[index]?.trim() || "";
+      return acc;
+    }, {});
+  });
+
+  return rows;
+};
+
+const addPreferToDB = async (csvData: CSVData[]) => {
+  const obj = { abc: 12 };
+  const formattedData: Preference[] = csvData.map(
+    (csv) =>
+      ({
+        material: csv["Material"],
+        form: csv["Form"],
+        grade: csv["Grade"],
+        choice: csv["Choice"],
+        min_width: parseFloat(csv["Width (Min)"]),
+        max_width: parseFloat(csv["Width (Max)"]),
+        min_thickness: parseFloat(csv["Thickness (Min)"]),
+        max_thickness: parseFloat(csv["Thickness (Max)"]),
+      })
+  );
+
+  await axios.post("http://localhost:4000/preference", formattedData);
+};
 
 export const UploadPrefer: React.FC = () => {
   const [csvData, setCsvData] = useState<CSVData[]>([]);
@@ -34,6 +55,7 @@ export const UploadPrefer: React.FC = () => {
         const parsedData = parseCSV(csvText);
 
         setCsvData(parsedData);
+        addPreferToDB(parsedData);
       };
       reader.readAsText(file);
     }
