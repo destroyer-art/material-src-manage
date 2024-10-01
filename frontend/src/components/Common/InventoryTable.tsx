6f5f2506c8d702d36629ddfd6870fc09482d3bc4
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2024-10-01 09:37:04 +0200

    feat: fetch inventory data with pagenation

--- frontend/src/components/Common/InventoryTable.tsx
+++ frontend/src/components/Common/InventoryTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
@@ -9,27 +9,25 @@ import {
   Paper,
   TablePagination,
 } from "@mui/material";
-
-// Define the row data type
-interface Row {
-  id: number;
-  name: string;
-  category: string;
-  price: number;
-}
-
-// Sample data
-const rows: Row[] = [
-  { id: 1, name: "Product A", category: "Category 1", price: 100 },
-  { id: 2, name: "Product B", category: "Category 2", price: 150 },
-  { id: 3, name: "Product C", category: "Category 1", price: 200 },
-  { id: 4, name: "Product D", category: "Category 3", price: 250 },
-  { id: 5, name: "Product E", category: "Category 2", price: 300 },
-];
+import { Inventory } from "../../types";
+import axios from "axios";
 
 export const InventoryTable: React.FC = () => {
-  const [page, setPage] = React.useState<number>(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState<number>(5);
+  const [inventory, setInventory] = useState<Inventory[]>([]);
+  const [page, setPage] = useState<number>(0);
+  const [rowsPerPage, setRowsPerPage] = useState<number>(10);
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:4000/inventory", {
+      params: { page: page + 1, perpage: rowsPerPage },
+    });
+    const data: Inventory[] = response.data;
+    return data;
+  };
+
+  useEffect(() => {
+    fetchData().then((data) => setInventory(data));
+  }, [page, rowsPerPage]);
 
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
@@ -44,25 +42,32 @@ export const InventoryTable: React.FC = () => {
 
   return (
     <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
+      <TableContainer>
         <Table stickyHeader aria-label="data table">
           <TableHead>
             <TableRow>
               <TableCell>ID</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Category</TableCell>
-              <TableCell>Price</TableCell>
+              <TableCell>Product Number</TableCell>
+              <TableCell>Form</TableCell>
+              <TableCell>Grade</TableCell>
+              <TableCell>Finish</TableCell>
+              <TableCell>Quantity</TableCell>
+              <TableCell>Weight</TableCell>
+              <TableCell>Location</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                  <TableCell>{row.id}</TableCell>
-                  <TableCell>{row.name}</TableCell>
-                  <TableCell>{row.category}</TableCell>
-                  <TableCell>{row.price}</TableCell>
+            {inventory
+              .map((ivntry) => (
+                <TableRow hover role="checkbox" tabIndex={-1} key={ivntry.id}>
+                  <TableCell>{ivntry.id}</TableCell>
+                  <TableCell>{ivntry.product_number}</TableCell>
+                  <TableCell>{`${ivntry.form} ${ivntry.choice}`}</TableCell>
+                  <TableCell>{`${ivntry.grade} ${ivntry.surface}`}</TableCell>
+                  <TableCell>{ivntry.finish}</TableCell>
+                  <TableCell>{ivntry.quantity}</TableCell>
+                  <TableCell>{ivntry.weight}</TableCell>
+                  <TableCell>{ivntry.location}</TableCell>
                 </TableRow>
               ))}
           </TableBody>
@@ -71,7 +76,7 @@ export const InventoryTable: React.FC = () => {
       <TablePagination
         rowsPerPageOptions={[5, 10, 15]}
         component="div"
-        count={rows.length}
+        count={11650}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
